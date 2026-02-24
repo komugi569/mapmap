0eba4d419c3689bca1f2e98de621946ec795c84b
komugi <junichirousagawa@gmail.com>
2026-02-24 21:25:38 +0900

    torikesi

--- vite-project/src/components/MapContainer.jsx
+++ vite-project/src/components/MapContainer.jsx
@@ -1,8 +1,6 @@
 import { useState, useRef } from "react";
 import Corridor from "./Corridor";
-import Room2_1 from "./rooms/Room2_1";
-import Room2_2 from "./rooms/Room2_2";
-import Room1_1 from "./rooms/Room1_1";
+import Room2_1 from "./rooms/Room2_1"
 
 export default function MapContainer() {
   const [scale, setScale] = useState(1);
@@ -30,8 +28,6 @@ export default function MapContainer() {
 
         {/* 教室配置 */}
         <Room2_1 x={300} y={400} />
-        <Room2_2 x={450} y={400} />
-        <Room1_1 x={800} y={600} />
 
       </div>
     </div>
